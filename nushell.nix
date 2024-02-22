97bd0563f02a47593b98b8c63fcddf2edd839032
Rasmus Lövegren <rasmus@lovegren.fi>
2024-02-22 09:44:29 +0200

    fix: update direnv hook

--- nushell.nix
+++ nushell.nix
@@ -4,10 +4,6 @@
   enable = true;
 
   extraConfig = ''
-<<<<<<< HEAD
-    $env.config = ($env.config | upsert show_banner false)
-    $env.config = ($env.config | upsert edit_mode vi)
-||||||| parent of 03f0edf (fix: don't include binutils as it provides ld from nixpkgs)
     let carapace_completer = {|spans| 
       carapace $spans.0 nushell $spans | from json
     }
@@ -31,31 +27,6 @@
         }
       }
     }
-=======
-    let carapace_completer = {|spans| 
-      carapace $spans.0 nushell $spans | from json
-    }
-
-    $env.config = {
-      show_banner: false,
-      edit_mode: vi,
-
-      hooks: {
-        pre_prompt: [{ ||
-          let direnv = (direnv export json | from json)
-          let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
-          $direnv | load-env
-        }]
-      },
-
-      completions: {
-        external: {
-          enable: true
-          completer: $carapace_completer
-        }
-      }
-    }
->>>>>>> 03f0edf (fix: don't include binutils as it provides ld from nixpkgs)
 
     alias da = direnv allow
     alias g = git
@@ -65,21 +36,7 @@
     }
     alias c = code-insiders
 
-<<<<<<< HEAD
-    # Maybe these won't be needed one day
-    alias ls = lsd
-    alias l = ls -l
-    alias la = ls -a
-    alias lla = ls -la
-    alias lt = ls --tree
-    alias ssh = ssh.exe
-    alias ssh-add = ssh-add.exe
-    alias scp = scp.exe
-||||||| parent of 03f0edf (fix: don't include binutils as it provides ld from nixpkgs)
-    let-env EDITOR = nvim
-=======
     $env.EDITOR = nvim
->>>>>>> 03f0edf (fix: don't include binutils as it provides ld from nixpkgs)
   '';
 
   extraEnv = ''
